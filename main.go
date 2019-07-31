e929975aa6e8ce3b151a81e8ead943269df4eea4
beingmode <53506164+beingmode@users.noreply.github.com>
2019-07-31 14:37:17 +0900

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "안녕 뚜니"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
